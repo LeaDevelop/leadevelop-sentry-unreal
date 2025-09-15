239c1aac2364ffd41ebbcd81448589883047e0e7
Lea N. - LeaDevelop <104503492+LeaDevelop@users.noreply.github.com>
2025-09-15 02:54:30 +0200

    feat: introduces tester that still needs to be tested and worked on

---
+++ LeaDevelopSentry/Source/LeaDevelopSentry/Private/LeaDevelopCrashTester.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2025 LeaDevelop. All Rights Reserved.
+// TODO still needs to be connected
+
+#include "LeaDevelopCrashTester.h"
+#include "Async/Async.h"
+#include "Misc/CommandLine.h"
+#include "Misc/Parse.h"
+#include "HAL/PlatformProcess.h"
+
+// Static members
+int32 FLeaDevelopCrashTester::CrashType = 0;
+
+void FLeaDevelopCrashTester::InitializeFromCommandLine()
+{
+#if !UE_BUILD_SHIPPING
+   FString CrashMeValue;
+   if (FParse::Value(FCommandLine::Get(), TEXT("CrashMe="), CrashMeValue))
+   {
+      int32 DelaySeconds = FCString::Atoi(*CrashMeValue);
+       
+      if (DelaySeconds > 0)
+      {
+         UE_LOG(LogTemp, Warning, TEXT("LeaDevelop: Crash test scheduled in %d seconds"), DelaySeconds);
+          
+         AsyncTask(ENamedThreads::AnyBackgroundThreadNormalTask, [DelaySeconds]()
+         {
+            FPlatformProcess::Sleep(DelaySeconds);
+            ExecuteCrashTest();
+         });
+      }
+   }
+#endif
+}
+
+void FLeaDevelopCrashTester::ExecuteCrashTest()
+{
+#if !UE_BUILD_SHIPPING
+   UE_LOG(LogTemp, Fatal, TEXT("LEADEVELOP CRASH TEST EXECUTING!"));
+   int32* NullPtr = nullptr;
+   *NullPtr = 42;
+#endif
+}
---
+++ LeaDevelopSentry/Source/LeaDevelopSentry/Public/LeaDevelopCrashTester.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2025 LeaDevelop. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class LEADEVELOPSENTRY_API FLeaDevelopCrashTester
+{
+public:
+    static void InitializeFromCommandLine();
+
+private:
+    static void ExecuteCrashTest();
+    static int32 CrashType;
+};
