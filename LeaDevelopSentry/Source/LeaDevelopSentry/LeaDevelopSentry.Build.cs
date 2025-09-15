e5d43e90ad14ffe966938adb95e3f66784c000fa
Lea N. - LeaDevelop <104503492+LeaDevelop@users.noreply.github.com>
2025-09-15 02:53:17 +0200

    updates build.cs needs further work

--- LeaDevelopSentry/Source/LeaDevelopSentry/LeaDevelopSentry.Build.cs
+++ LeaDevelopSentry/Source/LeaDevelopSentry/LeaDevelopSentry.Build.cs
@@ -1,56 +1,31 @@
-﻿// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright (c) 2025 LeaDevelop. All Rights Reserved.
+
+// TODO revise the list
 
 using UnrealBuildTool;
 
 public class LeaDevelopSentry : ModuleRules
 {
-	public LeaDevelopSentry(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				"Core",
-				"CoreUObject",
-				"Engine",
-				"Sentry" // ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+    public LeaDevelopSentry(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "Sentry"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "EngineSettings",
+                "Projects"
+            }
+        );
+    }
 }
