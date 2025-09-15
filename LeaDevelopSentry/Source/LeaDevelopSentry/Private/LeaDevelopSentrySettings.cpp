5638333d98d25a29c2c4deb72fa43b28dcd2dbe7
Lea N. - LeaDevelop <104503492+LeaDevelop@users.noreply.github.com>
2025-09-15 02:55:15 +0200

    feat: custom settings confirmed working

---
+++ LeaDevelopSentry/Source/LeaDevelopSentry/Private/LeaDevelopSentrySettings.cpp
@@ -0,0 +1,50 @@
+﻿#include "LeaDevelopSentrySettings.h"
+#include "SentrySubsystem.h"
+#include "Misc/EngineVersion.h"
+#include "Engine/Engine.h"
+
+void ULeaDevelopSentrySettings::ApplyCustomTags(USentrySubsystem* Sentry) const
+{
+	if (!Sentry || !Sentry->IsEnabled()) return;
+
+	TMap<FString, FSentryVariant> CustomContext;
+    
+	if (bPromoteChangelist)
+	{
+		FString Changelist = FString::FromInt(FEngineVersion::Current().GetChangelist());
+		Sentry->SetTag(TEXT("Changelist"), Changelist);
+		CustomContext.Add(TEXT("Changelist"), Changelist);
+	}
+    
+	if (bPromoteEngineVersion)
+	{
+		FString EngineVersion = FEngineVersion::Current().ToString(EVersionComponent::Patch);
+		Sentry->SetTag(TEXT("EngineVersion"), EngineVersion);
+		CustomContext.Add(TEXT("Engine Version"), EngineVersion);
+	}
+    
+	if (bPromoteLevelName && GWorld)
+	{
+		FString LevelName = GWorld->GetMapName();
+		LevelName.RemoveFromStart(GWorld->StreamingLevelsPrefix);
+		Sentry->SetTag(TEXT("LevelName"), LevelName);
+		CustomContext.Add(TEXT("Level Name"), LevelName);
+	}
+
+	if (!CustomContext.IsEmpty())
+	{
+		Sentry->SetContext(TEXT("LeaDevelop"), CustomContext);
+	}
+}
+
+#if WITH_EDITOR
+void ULeaDevelopSentrySettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+    
+	if (USentrySubsystem* Sentry = GEngine->GetEngineSubsystem<USentrySubsystem>())
+	{
+		ApplyCustomTags(Sentry);
+	}
+}
+#endif
---
+++ LeaDevelopSentry/Source/LeaDevelopSentry/Public/LeaDevelopSentrySettings.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "LeaDevelopSentrySettings.generated.h"
+
+class USentrySubsystem;
+
+UCLASS(Config=Engine, DefaultConfig, meta=(DisplayName="LeaDevelop Sentry"))
+class LEADEVELOPSENTRY_API ULeaDevelopSentrySettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	virtual FName GetCategoryName() const override { return TEXT("Plugins"); }
+	virtual FName GetSectionName() const override { return TEXT("LeaDevelop Sentry"); }
+
+	UPROPERTY(Config, EditAnywhere, Category = "Custom Tags")
+	bool bPromoteChangelist = true;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Custom Tags")
+	bool bPromoteEngineVersion = true;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Custom Tags")
+	bool bPromoteLevelName = true;
+
+	void ApplyCustomTags(USentrySubsystem* Sentry) const;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+};
